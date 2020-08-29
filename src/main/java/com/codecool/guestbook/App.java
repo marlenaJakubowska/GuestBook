6b61d8f4d0e4d05be91dcea1e527bd1191b4e709
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-08-29 18:34:17 +0200

    Create server

---
+++ src/main/java/com/codecool/guestbook/App.java
@@ -0,0 +1,23 @@
+package com.codecool.guestbook;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+        // create a server on port 8000
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        // set routes
+        server.createContext("/guestbook", new GuestBook());
+        server.createContext("/static", new Static());
+        server.setExecutor(null); // creates a default executor
+
+        // start listening
+        server.start();
+    }
+
+}
